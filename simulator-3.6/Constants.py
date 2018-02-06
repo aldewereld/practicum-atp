a9183c89121d1ea30af879e1787ef5bb968c0045
Huib Aldewereld <huib.aldewereld@hu.nl>
2018-02-06 19:46:11 +0100

    Updating simulator-code; added some addition (requested) features; cleaned up most of the 'magic numbers'.

--- simulator-3.6/Constants.py
+++ simulator-3.6/Constants.py
@@ -15,7 +15,7 @@ levelConversion = 0.5       # V/cm; 0V is empty
 colourConversion = 0.033 # 0.00V = value 0 (pitch black) - 3.3 V = value 100 (bright white); value/lightness score (HSV)
 
 """Set Points: these indicate the desired values for the dimensions of the resulting mixture"""
-levelSetPoint = 1.6 # cm liquid (0.8 = 500ml)
+levelSetPoint = 0.4 # cm liquid (0.8 = 500ml)
 colourSetPoint = 1.65 # % value
 tempSetPoint = 2.0 # degrees celsius
 
@@ -23,3 +23,32 @@ tempSetPoint = 2.0 # degrees celsius
 tempReaction = 0.05
 levelReaction = 0.07
 colourReaction = 0.05
+
+
+"""GUI Constants"""
+screensize = (1024, 768)
+margin = 20
+buttonWidth = 160
+buttonHeight = 30
+
+buttonPos = (screensize[0]-buttonWidth-margin, margin)
+simPos = (100, 100)
+
+ORANGE = (240, 120, 0)
+WHITE = (240, 240, 240)
+BLACK = (0, 0, 0)
+RED = (240, 0, 0)
+GREEN = (0, 240, 0)
+BLUE = (0, 0, 240)
+YELLOW = (240, 240, 0)
+
+graphX, graphY, graphW, graphH, graphM = 30, screensize[1]/2, 170, 140, 50
+graphs = {"temp": (graphX, graphY, graphW, graphH, RED),
+          "heater": (graphX, graphY + (graphM + graphH), graphW, graphH, (120, 120, 0)),
+          "colour": (graphX + (graphM + graphW), graphY, graphW, graphH, BLUE),
+          "level": (graphX + (graphM + graphW), graphY + graphM + graphH, graphW, graphH, GREEN),
+          "pumpA": (graphX + 2 * (graphM + graphW), graphY, graphW, graphH, (0, 120, 120)),
+          "valveA": (graphX + 3 * (graphM + graphW), graphY, graphW, graphH, (0, 120, 120)),
+          "pumpB": (graphX + 2 * (graphM + graphW), graphY + (graphM + graphH), graphW, graphH, (120, 0, 120)),
+          "valveB": (graphX + 3 * (graphM + graphW), graphY + (graphM + graphH), graphW, graphH, (120, 0, 120))
+          }
